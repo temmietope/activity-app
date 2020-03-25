08fc9898491032ca9d414be397ad3a29fe43528f
Temitope <temmieayodele@gmail.com>
2020-03-25 20:22:36 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import { StyleSheet, Text, View, FlatList } from "react-native";
+import Header from "./components/header";
+
+export default function App() {
+  const [todos, setTodos] = useState([
+    { text: "buy coffee", key: "1" },
+    { text: "create an app", key: "2" },
+    { text: "play on the switch", key: "3" }
+  ]);
+  return (
+    <View style={styles.container}>
+      <Header />
+      <View style={styles.content}>
+        {/* form */}
+        <View style={styles.list}>
+          <FlatList
+            data={todos}
+            renderItem={({ item }) => <Text>{item.text}</Text>}
+          />
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff"
+  },
+  content: {
+    padding: 40
+  },
+  list: {
+    marginTop: 40
+  }
+});
